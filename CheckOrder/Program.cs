b47e38bcd35f97add4c9ecd618e13d2d57445a2b
Abdulrahman_Aljafar <abdulrahmana.jafar@gmail.com>
2021-04-19 05:50:09 +0300

    Homework week two Day one

---
+++ CheckOrder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheckOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(checkOrder("1234554321"));
+            Console.WriteLine(checkOrder("9595"));
+            Console.WriteLine(checkOrder("89339998"));
+        }
+
+        static bool checkOrder(string value)
+        {
+            Stack<char> stack = new Stack<char>();
+            for(int i = 0;i< value.Length;i++)
+            {
+                if (stack.ToArray().Length == 0 || value[i] != stack.Peek())
+                {
+                    stack.Push(value[i]);
+                }
+                else
+                {
+                    stack.Pop();
+                }
+               
+            }
+            if (stack.ToArray().Length == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
